736f0607a47e4a480920a1b8b3fa72fc091882fd
Sergey <sinzem@gmail.com>
2024-10-11 19:07:40 +0300

    first

---
+++ description.js
@@ -0,0 +1,8 @@
+// npx create-react-app my-app --template typescript - приложение в ui-testing-progect
+
+// npm init -y - инициализируем папку ui-kit
+// npm i -D typescript ts-loader - там же ставим зависимости для работы, ts-loader понадобится для конфигурации wp
+// npm i -D react-dom , в package.json в ui-kit добавляем его в peerDependencies(зависимости, которые будут нужны только для взаимодействия со смежными проектами - ui-testing-progect в д.с)
+
+
+
